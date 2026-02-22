78d053228491eef797a7c07ecfd9d318b887186f
Piyushgarg06 <erpiyushgarg17@gmail.com>
2026-02-23 04:39:28 +0530

    trained a simple diffusion model to predict noise associated with data at timestep t

---
+++ day5.py
@@ -0,0 +1,57 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+
+T=1000
+betas=torch.linspace(1e-4,0.02,T)
+alphas=1.0-betas
+alpha_bar=torch.cumprod(alphas,dim=0)
+
+def generate_sparse_batch(batch_size=16,size=32):
+    x=torch.zeros(batch_size,1,size,size)
+    for i in range(batch_size):
+        cx , cy = torch.randint(0,size,(2,))
+        x[i,0,cx,cy] = torch.rand(1)*5
+    return x 
+
+class smallDiffusionModel(nn.Module):
+    def __init__(self,T,time_dim=32):
+        super().__init__()
+        self.time_embed = nn.Embedding(T,time_dim)
+        self.time_mlp = nn.Linear(time_dim,16)
+        self.conv1=nn.Conv2d(1,16,3,padding=1)
+        self.conv2=nn.Conv2d(16,1,3,padding=1)
+    def forward(self,x,t):
+        h=self.conv1(x)
+
+        time_emb = self.time_embed(t)
+        time_emb = self.time_mlp(time_emb)
+        time_emb = time_emb.unsqueeze(-1).unsqueeze(-1)
+
+        h=h+time_emb
+        h=torch.relu(h)
+        
+        out=self.conv2(h)
+        return out
+model = smallDiffusionModel(T)
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
+
+for step in range(200):
+    x0 = generate_sparse_batch()
+    batch = x0.shape[0]
+
+    t=torch.randint(0,T,(batch,))
+    epsillon = torch.randn_like(x0)
+    alpha_t = alpha_bar[t].view(batch,1,1,1)
+    x_t = torch.sqrt(alpha_t)*x0 + torch.sqrt(1-alpha_t)*epsillon
+    pred_noise = model(x_t,t)
+    loss = F.mse_loss(pred_noise,epsillon)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if step % 20 == 0:
+        print(f"Step {step} | Loss: {loss.item():.4f}")
+    
+
+
